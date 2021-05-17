f4f11490eb0613565233543ec219251afdd42211
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-05-17 12:47:51 +0530

    Added file of UC2_MULTIPLE_RIDES

--- CabInvoicee/Program.cs
+++ CabInvoicee/Program.cs
@@ -22,51 +22,153 @@ namespace CabInvoicee
                 INVALID_DISTANCE, INVALID_TIME, NULL_RIDES, INVALID_USER_ID
             }
         }
-        public class InvoiceGenerator
+    public class InvoiceGenerator
+    {
+
+        //Create Variables 
+        private RideType rideRepository; // created a variable with datatype as ridetype
+                                         //Create Constants
+        private readonly double MINIMUM_COST_PER_KM;
+        private readonly int COST_PER_TIME;
+        private readonly double MINIMUM_FARE;
+
+        //Initializes a new instance of the class.
+        //Creating Method
+        public InvoiceGenerator()
         {
+            this.rideRepository = new RideType(); // initiliziling all the variables using constructor 
+            this.MINIMUM_COST_PER_KM = 10;
+            this.COST_PER_TIME = 1;
+            this.MINIMUM_FARE = 5;
+        }
 
-            //Create Variables 
-            private RideType rideRepository; // created a variable with datatype as ridetype
-                                             //Create Constants
-            private readonly double MINIMUM_COST_PER_KM;
-            private readonly int COST_PER_TIME;
-            private readonly double MINIMUM_FARE;
-
-            //Initializes a new instance of the class.
-            //Creating Method
-            public InvoiceGenerator()
+        // Calculates the fare.     
+        // Create Method
+        // Invalid Time
+        public double CalculateFare(double distance, int time)
+        {
+            double totalFare = 0;
+            try // exception handling to calculate total fare
             {
-                this.rideRepository = new RideType(); // initiliziling all the variables using constructor 
-                this.MINIMUM_COST_PER_KM = 10;
-                this.COST_PER_TIME = 1;
-                this.MINIMUM_FARE = 5;
+                totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
             }
-
-            // Calculates the fare.     
-            // Create Method
-            // Invalid Time
-            public double CalculateFare(double distance, int time)
+            catch (CabInvoiceException) // it is user defined exception which is use to catch the exception
             {
-                double totalFare = 0;
-                try // exception handling to calculate total fare
+                if (distance <= 0)
                 {
-                    totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_DISTANCE, "Invalid Distance");
                 }
-                catch (CabInvoiceException) // it is user defined exception which is use to catch the exception
+                if (time <= 0)
                 {
-                    if (distance <= 0)
-                    {
-                        throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_DISTANCE, "Invalid Distance");
-                    }
-                    if (time <= 0)
-                    {
-                        throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_TIME, "Invalid Time");
-                    }
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_TIME, "Invalid Time");
                 }
-                return Math.Max(totalFare, MINIMUM_FARE); // returnig maximum between totalfare and min. fare..
+            }
+            return Math.Max(totalFare, MINIMUM_FARE); // returnig maximum between totalfare and min. fare..
+        }
+        // Calculates the fare for array of rides
+        // for checking total fare
+        // Adding Method 
+        public InvoiceSummary CalculateFare(Ride[] rides)
+        {
+            double totalFare = 0;
+            // checks for rides available and passes them to calculate fare method to calculate fare for each method
+            try
+            {
+                //calculating total fare for all rides
+                foreach (Ride ride in rides)
+                {
+                    totalFare += this.CalculateFare(ride.distance, ride.time);
+                }
+            }
+            //catches exception if available
+            catch (CabInvoiceException)
+            {
+                //If no rides there then throw exception
+                if (rides == null)
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.NULL_RIDES, "no rides found");
+                }
+            }
+            //returns invoice summary object 
+            return new InvoiceSummary(rides.Length, totalFare);
+        }
+
+        // Adds the rides in dictionary with key as a user id 
+        //Adding Method
+        public void AddRides(string userId, Ride[] rides)
+        {
+            try
+            {
+                rideRepository.AddRide(userId, rides);
+            }
+            catch (CabInvoiceException)
+            {
+                if (rides == null)
+                {
+                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.NULL_RIDES, "Null rides");
+                }
             }
         }
-        public class Ride
+        //Gets the invoice summary by passing user id into ride repository and then passing rides array to calculate fares.
+
+        public InvoiceSummary GetInvoiceSummary(string userId)
+        {
+            try
+            {
+                return this.CalculateFare(rideRepository.GetRides(userId));
+            }
+            catch
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_USER_ID, "Invalid user id");
+            }
+        }
+
+
+    }
+    /// <summary>
+    /// Generate Invoice summery
+    /// Invoice summary generates number of rides And  total fare
+    /// And Also generates average fare for summary
+    /// Create constructor and pass 3 variables of private type
+    /// </summary>
+    public class InvoiceSummary
+    {
+        private int numberOfRides;
+        private double totalFare;
+        private double averageFare;
+
+
+        // Initializes a new instance of the class.
+        // initializes number of rides, total fare and generates average fare for rides.
+        //Creating Parameterised constructor for numberofrides and totalfare
+        //getting value for that element
+        public InvoiceSummary(int numberOfRides, double totalFare)
+        {
+            this.numberOfRides = numberOfRides;
+            this.totalFare = totalFare;
+            this.averageFare = totalFare / numberOfRides;
+        }
+
+        // Determines whether the specified ,is equal to this instance.
+        //to compare with this instance.</param>
+        // <returns>
+        //  value isntrue if the specified  is equal to this instance; otherwise, <c>false</c>.
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is InvoiceSummary))
+            {
+                return false;
+            }
+            InvoiceSummary imputedObject = (InvoiceSummary)obj;
+            return this.numberOfRides == imputedObject.numberOfRides && this.totalFare == imputedObject.totalFare && this.averageFare == imputedObject.averageFare;
+        }
+    }
+    public class Ride
         {
             //Creating Variables
             public double distance;
@@ -136,20 +238,27 @@ namespace CabInvoicee
             }
         }
         public class Program
+    {
+           
+    /// Defines Entry point of application
+    /// </summary>
+    /// <param name="args"></param>
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-                Console.WriteLine("**********Welcome to Cab Invoice Program***********");
-                //Creating Object
-                InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-                //Calculate Fare double
-                double fare = invoiceGenerator.CalculateFare(2.0, 5);
-                Console.WriteLine($"Fare: {fare}");
-                Console.ReadLine();
+            Console.WriteLine("**********Welcome to Cab Invoice Program***********");
+            //Creating Object
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            //Calculate Fare double
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare: {fare}");
+            InvoiceSummary invoiceTotalFare = new InvoiceSummary(3, 5);
+            Console.WriteLine(invoiceTotalFare);
+            Console.ReadLine();
 
-            }
         }
-    
+    }
 }
+    
+
 
 
