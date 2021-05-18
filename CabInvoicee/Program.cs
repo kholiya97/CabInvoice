f41511c1850aebd0cd212429f71a780c25bd963c
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-05-18 20:28:13 +0530

    Added file of UC5_PREMIUM_RIDE_SERVICE

--- CabInvoicee/Program.cs
+++ CabInvoicee/Program.cs
@@ -6,53 +6,78 @@ using System.Threading.Tasks;
 
 namespace CabInvoicee
 {
-       public class CabInvoiceException : Exception // extended used concept of inhertence by extending exception class
+    /// <summary>
+    /// Custom exception for cab invoice program
+    /// </summary>
+    public class CabInvoiceException : Exception
+    {
+        // Enum for defining different type of custom exception       
+        public ExceptionType type;
+
+        // Initializes a new instance of the class.
+
+        public CabInvoiceException(ExceptionType type, string message) : base(message)
         {
-            // Enum for defining different type of custom exception     
-            public ExceptionType type;
-
-            // Initializes a new instance of the class.
-
-            public CabInvoiceException(ExceptionType type, string message) : base(message) // i am calling baseclass constructor and giving appropriate message
-            {
-                this.type = type;
-            }
-            public enum ExceptionType // created a method with return type enum.
-            {
-                INVALID_DISTANCE, INVALID_TIME, NULL_RIDES, INVALID_USER_ID
-            }
+            this.type = type;
         }
+        public enum ExceptionType
+        {
+            INVALID_DISTANCE, INVALID_TIME, NULL_RIDES, INVALID_USER_ID, INVALID_RIDETYP
+        }
+    }
     public class InvoiceGenerator
     {
 
         //Create Variables 
-        private RideType rideRepository; // created a variable with datatype as ridetype
-                                         //Create Constants
+        private RideRepository rideRepository;
+        RideType rideType;
+        //Create Constants
         private readonly double MINIMUM_COST_PER_KM;
         private readonly int COST_PER_TIME;
         private readonly double MINIMUM_FARE;
 
         //Initializes a new instance of the class.
         //Creating Method
-        public InvoiceGenerator()
+        /// Initializes a new instance of the <see cref="InvoiceGenerator"/> class.
+        /// </summary>
+        public InvoiceGenerator(RideType rideType)
         {
-            this.rideRepository = new RideType(); // initiliziling all the variables using constructor 
-            this.MINIMUM_COST_PER_KM = 10;
-            this.COST_PER_TIME = 1;
-            this.MINIMUM_FARE = 5;
+            this.rideRepository = new RideRepository();
+            this.rideType = rideType;
+            try
+            {
+                if (this.rideType.Equals(RideType.NORMAL))
+                {
+                    this.MINIMUM_COST_PER_KM = 10;
+                    this.COST_PER_TIME = 1;
+                    this.MINIMUM_FARE = 5;
+                }
+                if (this.rideType.Equals(RideType.PREMIUM))
+                {
+                    this.MINIMUM_COST_PER_KM = 15;
+                    this.COST_PER_TIME = 2;
+                    this.MINIMUM_FARE = 20;
+                }
+            }
+            catch (CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_RIDETYP, "invalid ride type");
+            }
+
+
         }
 
         // Calculates the fare.     
-        // Create Method
         // Invalid Time
+        //Create Parameterised Constructor passes value distance and time
         public double CalculateFare(double distance, int time)
         {
             double totalFare = 0;
-            try // exception handling to calculate total fare
+            try
             {
                 totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
             }
-            catch (CabInvoiceException) // it is user defined exception which is use to catch the exception
+            catch (CabInvoiceException)
             {
                 if (distance <= 0)
                 {
@@ -63,8 +88,9 @@ namespace CabInvoicee
                     throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_TIME, "Invalid Time");
                 }
             }
-            return Math.Max(totalFare, MINIMUM_FARE); // returnig maximum between totalfare and min. fare..
+            return Math.Max(totalFare, MINIMUM_FARE);
         }
+
         // Calculates the fare for array of rides
         // for checking total fare
         // Adding Method 
@@ -122,8 +148,6 @@ namespace CabInvoicee
                 throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_USER_ID, "Invalid user id");
             }
         }
-
-
     }
     /// <summary>
     /// Generate Invoice summery
@@ -168,97 +192,99 @@ namespace CabInvoicee
             return this.numberOfRides == imputedObject.numberOfRides && this.totalFare == imputedObject.totalFare && this.averageFare == imputedObject.averageFare;
         }
     }
-    public class Ride
-        {
-            //Creating Variables
-            public double distance;
-            public int time;
-
-            //Initializes a new instance of the Ride class.
-            //getting values of distance and times
-            public Ride(double distance, int time)
-            {
-                this.distance = distance;
-                this.time = time;
-            }
-        }
-        public class RideType
-        {
-            //Dictionary to store UserId and Rides int List. 
-            Dictionary<string, List<Ride>> userRides = null;
-
-            /// <summary>
-            /// Initializes a new instance of the <see cref="RideRepository"/> class.
-            /// </summary>
-            public RideType() // creating a instance of ridetype assigning values to dictionary userrides with key as string and value as list of distance and time.
-            {
-                this.userRides = new Dictionary<string, List<Ride>>();
-            }
-
-            /// <summary>
-            /// Adds ride of a customer in list and then to a dictionary with user id as a key.
-            /// </summary>
-            /// <param name="userId">The user identifier.</param>
-            /// <param name="rides">The rides array with datatype ride</param>
-            /// <exception cref="CabInvoiceDay23.CabInvoiceException">Rides are null</exception>
-            public void AddRide(string userId, Ride[] rides) // created method to add rides in a list
-            {
-                bool rideList = this.userRides.ContainsKey(userId);// created a boolean variable ridelist to check if the user id is present in list.
-                try
-                {
-                    if (!rideList) // if it in not present in list then create a new list and add in the dictonary 
-                    {
-                        List<Ride> list = new List<Ride>();
-                        list.AddRange(rides); // adding rides in list 
-                        this.userRides.Add(userId, list); // adding user id and list in dictonary
-                    }
-                }
-                catch
-                {
-                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.NULL_RIDES, "Rides are null"); // if there are no rides then throw this exception
-                }
-            }
-
-            /// <summary>
-            /// Gets the rides for specific user id.
-            /// </summary>
-            /// <param name="userId">The user identifier.</param>
-            /// <returns> retruns the array of all the rides of user in form of array</returns>
-            /// <exception cref="CabInvoiceDay23.CabInvoiceException">Invalid User id</exception>
-            public Ride[] GetRides(string userId) // created method for getrides
-            {
-                try
-                {
-                    return this.userRides[userId].ToArray(); // returning all the rides with specified user id
-                }
-                catch (CabInvoiceException)
-                {
-                    throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_USER_ID, "Invalid User id"); // if not throw error 
-                }
-            }
-        }
-        public class Program
-    {
-           
-    /// Defines Entry point of application
+    /// <summary>
+    /// Ride class to set data for particular ride. 
     /// </summary>
-    /// <param name="args"></param>
+    public class Ride
+    {
+        //Creating Variables
+        public double distance;
+        public int time;
+
+        //Initializes a new instance of the Ride class.
+        //getting values of distance and times
+        public Ride(double distance, int time)
+        {
+            this.distance = distance;
+            this.time = time;
+        }
+    }
+    public class RideRepository
+    {
+        //Dictionary to store UserId and Rides int List.
+        Dictionary<string, List<Ride>> userRides = null;
+        /// Initializes a new instance of the  class.
+       // for getting element
+        public RideRepository()
+        {
+            this.userRides = new Dictionary<string, List<Ride>>();
+        }
+
+        //Adding Method AddRide
+        // Adding ride of a customer in list and then to a dictionary with user id as a key.
+        public void AddRide(string userId, Ride[] rides)
+        {
+            //Boolean variable for ridelist
+            bool rideList = this.userRides.ContainsKey(userId);
+            try
+            {
+                //If there is not ridelist 
+                if (!rideList)
+                {
+                    //Creating list of having name list 
+                    //creating list of class ride
+                    List<Ride> list = new List<Ride>();
+                    //Adds collection of element in list
+                    list.AddRange(rides);
+                    //getting D
+                    this.userRides.Add(userId, list);
+                }
+            }
+            catch
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.NULL_RIDES, "Rides are null");
+            }
+        }
+
+        // Gets the rides for specific user id.     
+        //The user identifier userID
+        //retruns the array of all the rides of user in form of array
+        public Ride[] GetRides(string userId)
+        {
+            try
+            {
+                return this.userRides[userId].ToArray();
+            }
+            catch (CabInvoiceException)
+            {
+                throw new CabInvoiceException(CabInvoiceException.ExceptionType.INVALID_USER_ID, "Invalid User id");
+            }
+        }
+    }
+    /// <summary>
+    /// creating enum to choose between two ride types- normal and premium
+    /// </summary>
+    public enum RideType
+    {
+        NORMAL, PREMIUM
+    }
+    public class Program
+    {/// <summary>
+     /// Defines Entry point of application
+     /// </summary>
+     /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("**********Welcome to Cab Invoice Program***********");
             //Creating Object
-            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
             //Calculate Fare double
             double fare = invoiceGenerator.CalculateFare(2.0, 5);
             Console.WriteLine($"Fare: {fare}");
-            InvoiceSummary invoiceTotalFare = new InvoiceSummary(3, 5);
-            Console.WriteLine(invoiceTotalFare);
+
             Console.ReadLine();
 
         }
     }
 }
-    
-
-
 
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -15,13 +15,13 @@ namespace UnitTestProject1
         public void GivenDistanceAndTimeShouldReturnTotalFare()
         {
             //Creting Object of InvoiceGenerator
-            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
             double distance = 2.0;
             int time = 5;
 
             //For calculating fare
             double fare = invoiceGenerator.CalculateFare(distance, time);
-            double expected = 25;
+            double expected = 40;
             Assert.AreEqual(expected, fare);
         }
         /// <summary>
@@ -31,12 +31,12 @@ namespace UnitTestProject1
         public void GivenMultipleRidesShouldReturnInvoiceSummary()
         {
             //Creating instance of invoice generator 
-            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
             Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
 
             //Generating Summary for rides
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
-            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 60.0);
 
             //Asserting values
             Assert.AreEqual(expectedSummary, summary);
@@ -48,16 +48,35 @@ namespace UnitTestProject1
         public void GivenMultipleRidesShouldReturnInvoiceSummarywithAverageFare()
         {
             //Creating instance of invoice generator 
-            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
             Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
 
             //Generating Summary for rides
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
-            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 60.0);
 
             //Asserting values with average in equals to formula in invoice summary
             Assert.AreEqual(expectedSummary, summary);
         }
-
+        /// <summary>
+        /// Givens the rides for different users should return invoice summary. UC4
+        /// </summary>
+        [TestMethod]
+        public void GivenRidesForDifferentUsersShouldReturnInvoiceSummary()
+        {
+            //Creating instance of invoice generator 
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+            string userId = "001";
+            invoiceGenerator.AddRides(userId, rides);
+            string userIdForSecondUser = "002";
+            Ride[] ridesForSecondUser = { new Ride(3.0, 10), new Ride(1.0, 2) };
+            invoiceGenerator.AddRides(userIdForSecondUser, ridesForSecondUser);
+            //Generating Summary for rides
+            InvoiceSummary summary = invoiceGenerator.GetInvoiceSummary(userId);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+            //Asserting values with average in equals to formula in invoice summary
+            Assert.AreEqual(expectedSummary, summary);
+        }
     }
 }
